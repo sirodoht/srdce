f5db3a3b627bf587586c88c5869a3cf7f53fda09
Theodore Keloglou <zf@sirodoht.com>
2021-03-06 20:56:09 +0000

    add mate active field

--- main/models.py
+++ main/models.py
@@ -12,6 +12,7 @@ class User(AbstractUser):
 class Mate(models.Model):
     name = models.CharField(max_length=300)
     joined_at = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
