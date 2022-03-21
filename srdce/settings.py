508dd5aff601082310af0677e1c3b3c2c51b8e37
Theodore Keloglou <zf@sirodoht.com>
2022-03-21 23:09:11 +0000

    fix static compilation setting

--- srdce/settings.py
+++ srdce/settings.py
@@ -132,7 +132,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "_static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # Security middleware
