0acf1b214e6064da8abdf97b0a118d85b6209a7c
Orfeas Antoniou <orfeas@orfeasa.com>
2022-04-10 21:24:06 +0100

    Force reload of static files on change

--- srdce/settings.py
+++ srdce/settings.py
@@ -131,6 +131,7 @@ USE_TZ = False
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 
 # Security middleware
