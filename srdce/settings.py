96bd203b5471da93ebfe3ea3f33473a30eed2c8d
Theodore Keloglou <zf@sirodoht.com>
2022-04-13 14:52:57 +0300

    disable static files manifest

--- srdce/settings.py
+++ srdce/settings.py
@@ -131,7 +131,6 @@ USE_TZ = False
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 
 # Security middleware
